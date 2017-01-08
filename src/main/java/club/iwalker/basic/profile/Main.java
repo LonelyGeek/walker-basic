40e4db1569362be6546881922cf4fe63fbfffdcf
iwangchen <iwangchen01@gmail.com>
2017-01-08 11:28:52 +0800

    profile

--- src/main/java/club/iwalker/basic/profile/Main.java
+++ src/main/java/club/iwalker/basic/profile/Main.java
@@ -5,17 +5,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 
 	public static void main(String[] args) {
-		  AnnotationConfigApplicationContext context =  
+
+		AnnotationConfigApplicationContext context =
 				  new AnnotationConfigApplicationContext();
+
+		//通过Environment 配置，还可以通过 jvm的spring.profiles.active和Servlet的context parameter配置
+		context.getEnvironment().setActiveProfiles("prod"); //1
+		context.register(ProfileConfig.class);//2
+		context.refresh(); //3
 		  
-		  context.getEnvironment().setActiveProfiles("prod"); //1
-		  context.register(ProfileConfig.class);//2
-		  context.refresh(); //3
-		  
-	      ProfileBean profileBean = context.getBean(ProfileBean.class);
+		ProfileBean profileBean = context.getBean(ProfileBean.class);
 	      
-	      System.out.println(profileBean.getContent());
+		System.out.println(profileBean.getContent());
 	      
-	      context.close();
+		context.close();
 	}
 }
